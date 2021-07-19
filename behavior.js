91c6dd8d3a12d0372724a3b141a35308f3fe8543
biruk1224 <biruksolomonkd08@gmail.com>
2021-07-19 18:18:16 -0400

    2nd commit

---
+++ behavior.js
@@ -0,0 +1,9 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar_menu');
+
+
+menu.addEventListener('click', function(){
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active')
+
+})
